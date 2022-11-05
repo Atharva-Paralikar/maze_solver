c5df31023bafbd88bd0c72a31760c2d3f04a3a3c
Atharva Paralikar <atharva.paralikar@gmail.com>
2022-11-05 02:01:15 -0400

    default arguments for command line

--- application/main.cpp
+++ application/main.cpp
@@ -4,12 +4,17 @@
 #include "simulator/simulator.h"
 #include "simulator/robot.h"
 
-int main(int argc, char *argv[]){
-
-    std::cerr << "The robot will follow the " << argv[1] <<
+int main(int argc , char *argv[]){
+    std::cerr<<argc<<std::endl;
+    std::string args;
+    if (argc == 1){
+        args = "left";
+    }
+    else{
+        args = argv[1];
+    }
+    std::cerr << "The robot will follow the " << args <<
                                          " following algorithm" <<std::endl;
-    std::string args(argv[1]);
-    std::cerr<< args.length() <<std::endl;
     rwa2::Robot robot;
     robot.robot_init();
     robot.search_maze(args);
